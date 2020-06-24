8acbfa66bf29794884ff6883c4f0682071dc0daa
EdgarLZ <edgarosv.lz@gmail.com>
2020-06-24 18:09:56 -0500

    Added use of StringBuilder and regular expression

--- palindrome/src/palindrome/Palindrome.java
+++ palindrome/src/palindrome/Palindrome.java
@@ -2,19 +2,8 @@ package palindrome;
 
 public class Palindrome {
 	public boolean isPalindrome(String str) {
-		
-		String punc = " .,;!-'?_\"";
-		String seq = "";
-		for(int i=0; i< str.length();i++) {
-			if(!punc.contains(""+str.charAt(i))) {
-				seq = seq+ str.charAt(i);
-			}
-		}
-		seq = seq.toLowerCase();
-		for(int i=0; i< seq.length();i++)
-			if(seq.charAt(i) !=seq.charAt(seq.length()-i-1))
-				return false;
-		
-		return true;
+		String pattern = "[^a-zA-Z]";
+		StringBuilder seq = new StringBuilder(str.toLowerCase().replaceAll(pattern,""));
+		return seq.toString().equals(seq.reverse().toString());
 	}
 }
