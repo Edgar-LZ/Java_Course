58245c9c4a466d9c6c431d289cb73597797e4cd2
EdgarLZ <edgarosv.lz@gmail.com>
2020-06-24 17:28:27 -0500

    Deleted Gcd class and implemented gcd2 method in GcdN class

--- gcd/src/gcd_five/GcdN.java
+++ gcd/src/gcd_five/GcdN.java
@@ -1,14 +1,19 @@
 package gcd_five;
 
 public class GcdN {
-	public int gcd(int... args) {
-		Gcd gcd2 = new Gcd();
+	public int gcdn(int... args) {
 		BubbleSort bubble = new BubbleSort();
 		int[] sorted = bubble.sort(args);
 		int divisor = sorted[0];
 		for(int x: sorted) {
-			divisor = gcd2.gcd(divisor,x);
+			divisor = gcd2(divisor,x);
 		}			
 		return divisor;
+	}
+	
+	public int gcd2(int a,int b) {
+		if(a==0)
+			return b;
+		return gcd2(b%a,a);
 	}
 }
--- gcd/src/gcd_five/GreatestCommonDivisor.java
+++ gcd/src/gcd_five/GreatestCommonDivisor.java
@@ -3,6 +3,6 @@ package gcd_five;
 public class GreatestCommonDivisor {
 	public static void main(String[] args) {
 		GcdN gcdn = new GcdN();
-		System.out.println(gcdn.gcd(30,6,9,27,63));
+		System.out.println(gcdn.gcdn(30,6,9,27,63));
 	}
 }
