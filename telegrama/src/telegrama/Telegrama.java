2d5149926dce4f7b6929e784321556459c02d678
EdgarLZ <edgarosv.lz@gmail.com>
2020-08-03 00:07:27 -0500

    change approach

--- telegrama/src/telegrama/Telegrama.java
+++ telegrama/src/telegrama/Telegrama.java
@@ -2,19 +2,16 @@ package telegrama;
 
 public class Telegrama {
 	public static void main(String[] args) {
-		String msg = "Hola!!  ";
-		int len = msg.length();
+		String msg = "Feliz aniversario 2!";
+		char[] chars = msg.toCharArray();
 		int price = 0;
-		for(int i=0;i<len;i++) {
-			int ascii = (int) msg.charAt(i);
-			if(ascii >= (int) '0' && ascii <= (int) '9' ) {
+		for(char c : chars) {
+			if(c >= '0' && c <= '9' ) {
 				price += 20;
-			} else if(ascii >= (int) 'a' && ascii <= (int) 'z' || ascii >= (int) 'A' && ascii <= (int) 'Z') {
+			} else if((c >= 'a' && c <= 'z') || (c >=  'A' && c <=  'Z')) {
 				price +=10;
 			}
-			else if(msg.charAt(i)==' ') {
-				price+=0;
-			}
+			else if(c == ' ');
 			else {
 				price+=30;
 			}
